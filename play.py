1c10ef87b29d5aea32ab7ea782f8260080d8ce37
Jorge Santiago <jsantiago12tone@gmail.com>
2022-12-06 02:33:25 -0300

    Import only what's used with the modules, and format the code with Black

--- play.py
+++ play.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 
-from mido import MidiFile, MetaMessage
-import sys
-from pathlib import Path
-from time import sleep
+import PySimpleGUI as GUI
 import ntpath
-import pyautogui
-from pyautogui import press
-import PySimpleGUI as sg
 import os.path
-import _thread
-import glob
+import sys
+from _thread import start_new_thread
+from glob import glob
+from mido import MidiFile, MetaMessage
+from pathlib import Path
+from pyautogui import PAUSE, press
+from time import sleep
+
 
 def note_to_frequency(note):
     """
     Convert a MIDI note into a frequency (given in Hz)
     """
     return round(440 * 2 ** ((note - 69) / 12))
+
 
 def frequency_to_key(frequency):
     """
@@ -30,56 +31,57 @@ def frequency_to_key(frequency):
         1319: "3",
         1175: "2",
         1047: "8",
-        988:  "7",
-        932:  "j",
-        880:  "6",
-        831:  "h",
-        784:  "5",
-        740:  "g",
-        698:  "4",
-        659:  "3",
-        622:  "f",
-        587:  "2",
-        554:  "d",
-        523:  "1",
-        494:  "t",
-        466:  "c",
-        440:  "r",
-        415:  "x",
-        392:  "e",
-        370:  "z",
-        349:  "w",
-        330:  "q",
-        311:  "l",
-        294:  "0",
-        277:  "k",
-        262:  "9",
-        247:  "s",
-        233:  ".",
-        220:  "a",
-        208:  "m",
-        196:  "p",
-        185:  "n",
-        175:  "o",
-        165:  "i",
-        156:  "b",
-        147:  "u",
-        139:  "v",
-        131:  "y"
+        988: "7",
+        932: "j",
+        880: "6",
+        831: "h",
+        784: "5",
+        740: "g",
+        698: "4",
+        659: "3",
+        622: "f",
+        587: "2",
+        554: "d",
+        523: "1",
+        494: "t",
+        466: "c",
+        440: "r",
+        415: "x",
+        392: "e",
+        370: "z",
+        349: "w",
+        330: "q",
+        311: "l",
+        294: "0",
+        277: "k",
+        262: "9",
+        247: "s",
+        233: ".",
+        220: "a",
+        208: "m",
+        196: "p",
+        185: "n",
+        175: "o",
+        165: "i",
+        156: "b",
+        147: "u",
+        139: "v",
+        131: "y",
     }
-    
-    return notes.get(frequency,
-    "\t\t keystroke NOT FOUND, frequency: " + str(frequency))
+
+    return notes.get(
+        frequency, "\t\t keystroke NOT FOUND, frequency: " + str(frequency)
+    )
 
 
 def read_files(folder):
-    files = glob.glob(os.path.join(folder, "*.mid*"))
+    files = glob(os.path.join(folder, "*.mid*"))
     file_names = [os.path.basename(file) for file in files]
     return file_names
 
 
 def play_midi(filename):
-    pyautogui.PAUSE = 0.05
+    PAUSE = 0.05
 
     # Import the MIDI file
     midi_file = MidiFile(filename)
@@ -98,7 +100,7 @@ def play_midi(filename):
         if stop == True:
             break
     refresh_window()
-    
+
 
 def refresh_window():
     window.refresh()
@@ -110,35 +112,31 @@ def refresh_window():
 # Left column
 file_list_column = [
     [
-        sg.Text("Select the songs directory"),
-        sg.In("", size=(25, 1), enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse(),
+        GUI.Text("Select the songs directory"),
+        GUI.In("", size=(25, 1), enable_events=True, key="-FOLDER-"),
+        GUI.FolderBrowse(),
     ],
-    [
-        sg.Listbox(
-            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
-        )
-    ],
+    [GUI.Listbox(values=[], enable_events=True, size=(40, 20), key="-FILE LIST-")],
 ]
 
 # Right column
 button_column = [
-    [sg.Text("Selected file:")],
-    [sg.Text(size=(40, 1), key="-TOUT-")],
-    [sg.Button('Play', enable_events=True, key="-PLAY-", disabled=True)],
-    [sg.Button('Stop', enable_events=True, key="-STOP-", disabled=True)]
+    [GUI.Text("Selected file:")],
+    [GUI.Text(size=(40, 1), key="-TOUT-")],
+    [GUI.Button("Play", enable_events=True, key="-PLAY-", disabled=True)],
+    [GUI.Button("Stop", enable_events=True, key="-STOP-", disabled=True)],
 ]
 
 # Full layout with
 layout = [
     [
-        sg.Column(file_list_column),
-        sg.VSeperator(),
-        sg.Column(button_column),
+        GUI.Column(file_list_column),
+        GUI.VSeperator(),
+        GUI.Column(button_column),
     ]
 ]
 
-window = sg.Window("BardLinux-Player", layout)
+window = GUI.Window("BardLinux-Player", layout)
 
 # Run the Event Loop
 
@@ -149,22 +147,20 @@ while True:
     stop = False
 
     # Exit the event loop if it meets these conditions
-    if event == "Exit" or event == sg.WIN_CLOSED:
+    if event == "Exit" or event == GUI.WIN_CLOSED:
         stop = True
         break
 
     # List the files in the directory
     if event == "-FOLDER-":
         window["-FILE LIST-"].update(read_files(values["-FOLDER-"]))
-        window["-TOUT-"].update('')
+        window["-TOUT-"].update("")
         window["-PLAY-"].update(disabled=True)
 
     # A file was chosen from the list
     elif event == "-FILE LIST-":
         try:
-            filename = os.path.join(
-                values["-FOLDER-"], values["-FILE LIST-"][0]
-            )
+            filename = os.path.join(values["-FOLDER-"], values["-FILE LIST-"][0])
             window["-TOUT-"].update(values["-FILE LIST-"][0])
             window["-PLAY-"].update(disabled=False)
         except:
@@ -173,7 +169,7 @@ while True:
     # Play button pressed
     elif event == "-PLAY-":
         window["-STOP-"].update(disabled=False)
-        _thread.start_new_thread(play_midi,(filename,))
+        start_new_thread(play_midi, (filename,))
 
     # Stop button pressed
     elif event == "-STOP-":
